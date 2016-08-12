50918d8819295aa2ef93db6422c8f5482e25ce0e
Enrico Murru <enrico.murru@gmail.com>
2016-08-12 18:12:26 +0200

    Update script.js

--- P_U0000001_P0000001/script.js
+++ P_U0000001_P0000001/script.js
@@ -1,10 +1,2 @@
 console.log('I\'ve been loaded!');
-
-$.get(
-				"https://raw.githubusercontent.com/Salesforce-Organizer/sforg-test-repo/master/P_U0000001_P0000001/script.js",
-				function(data){
-				  console.log(data);
-				}
-				);
-				
-console.log(PopupUtils.StandardLoginURLs);
+$Msg.echo('Hello world!');
