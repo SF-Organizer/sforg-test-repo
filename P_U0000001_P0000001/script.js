34efc18809513edf58d0ea690b13f88349246d26
Enrico Murru <enrico.murru@gmail.com>
2016-08-12 15:34:09 +0200

    Update script.js

--- P_U0000001_P0000001/script.js
+++ P_U0000001_P0000001/script.js
@@ -1,1 +1,8 @@
 console.log('I\'ve been loaded!');
+
+$.get(
+				"https://raw.githubusercontent.com/Salesforce-Organizer/sforg-test-repo/master/P_U0000001_P0000001/script.js",
+				function(data){
+				  console.log(data);
+				}
+				);
